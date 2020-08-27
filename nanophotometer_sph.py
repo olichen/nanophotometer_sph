b4f547df4fddcacc1abc67d8ff336ee1cbef46a3
Oliver Chen <olichen@ucdavis.edu>
2020-08-27 16:41:13 -0700

    working connection to nanophotometer

--- nanophotometer_sph.py
+++ nanophotometer_sph.py
@@ -1,1 +1,24 @@
-print('hello world')
+import socketio
+import requests
+import json
+
+sio = socketio.Client()
+uri = 'http://192.168.1.31'
+
+@sio.event
+def connect():
+    print('connected')
+
+@sio.event
+def message(data):
+    print('message received', data)
+    if 'ready' in data and data['ready'] == 'sample':
+        response = requests.get(uri + '/rest/session/sample')
+        print(response.text)
+
+@sio.event
+def disconnect():
+    print('disconnected')
+
+sio.connect(uri + ':8765')
+sio.wait()
