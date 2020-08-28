b2a50a320c4d7067337e537a373e9ab79490ddbc
Oliver Chen <olichen@ucdavis.edu>
2020-08-28 15:43:47 -0700

    working demo

--- nanophotometer_sph.py
+++ nanophotometer_sph.py
@@ -1,24 +1,76 @@
 import socketio
 import requests
 import json
+import mysql.connector
+from getpass import getpass
 
-sio = socketio.Client()
-uri = 'http://192.168.1.31'
 
-@sio.event
-def connect():
-    print('connected')
+class NanophotometerNamespace(socketio.ClientNamespace):
+    def __init__(self, uri, sql) -> None:
+        super().__init__()
+        self.uri = uri
+        self.sql = sql
 
-@sio.event
-def message(data):
-    print('message received', data)
-    if 'ready' in data and data['ready'] == 'sample':
-        response = requests.get(uri + '/rest/session/sample')
-        print(response.text)
+    def on_connect(self) -> None:
+        print('connected')
 
-@sio.event
-def disconnect():
-    print('disconnected')
+    def on_connection_error(self) -> None:
+        print('connection error')
 
-sio.connect(uri + ':8765')
-sio.wait()
+    def on_disconnect(self) -> None:
+        print('disconnected')
+
+    def on_message(self, data: dict):
+        print('message received', data)
+        if 'ready' in data and data['ready'] == 'sample':
+            response = requests.get(self.uri + '/rest/session/sample')
+            sql.updateDatabase(response.text)
+            print(response.text)
+
+class MySQLConnection:
+    def __init__(self, host: str, user: str, pw: str) -> None:
+        self.host = host
+        self.user = user
+        self.pw = pw
+        self.db = 'etonbioscience'
+        cnx = mysql.connector.connect(user=self.user, password=self.pw,
+                host=self.host, database=self.db)
+        cnx.close()
+
+    def updateDatabase(self, data: str) -> bool:
+        sample = json.loads(data)
+        conc = sample['c']
+        s = conc + 1
+        p = conc + 2
+        h = conc + 3
+        o_num = 960254 # test order
+        s_num = 1
+        try:
+            cnx = mysql.connector.connect(user=self.user, password=self.pw,
+                    host=self.host, database=self.db)
+        except mysql.connector.Error as e:
+            print(e)
+        else:
+            cursor = cnx.cursor()
+            query = ("UPDATE sampletable "
+                    f"SET SampleCntr = '{conc}', S = '{s}', P = '{p}', H = '{h}' "
+                    f"WHERE OrderNumber = '{o_num}' AND SampleID = '{s_num}'")
+            print(query)
+            cursor.execute(query)
+            cnx.close()
+
+if __name__ == '__main__':
+    host = input('SQL server host > ')
+    user = input('username > ')
+    pw = getpass('password > ')
+    sql = MySQLConnection(host, user, pw)
+
+    ip = input('Nanophotometer ip address > ')
+    ip = ip or '192.168.1.31'
+    uri = 'http://' + ip
+    sio = socketio.Client()
+
+    sio.register_namespace(NanophotometerNamespace(uri, sql))
+    sio.connect(uri + ':8765')
+
+    sio.wait()
